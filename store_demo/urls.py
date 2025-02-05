7c07a14662f641ea14f497e921aa9bb7fedcae1b
Subhanu Majumder <reach.subhanu@gmail.com>
2025-02-05 22:29:26 +0530

    change allauth url

--- store_demo/urls.py
+++ store_demo/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("allauth.urls")),
+    path("allauth/", include("allauth.urls")),
 ]
